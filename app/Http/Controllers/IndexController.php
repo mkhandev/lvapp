87829f6adbf5f349e327d42d50b5c2c56d49a942
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2024-08-24 21:06:05 +0600

    add docker

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index(){
+
+        dd(Listing::all());
+
         return inertia("Index/Index",[
             'message' => "This is home page"
         ]);
