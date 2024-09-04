b90e4396b61c542018d32020e08af3fdd61277b4
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2024-09-04 16:46:53 +0600

    Email verification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Policies\NotificationPolicy;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +24,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::policy(DatabaseNotification::class, NotificationPolicy::class);// Manual registration policy
+        Gate::policy(DatabaseNotification::class, NotificationPolicy::class); // Manual registration policy
+
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+
+            $mailMessage =  (new MailMessage)
+                ->subject('Verify Email Address')
+
+                ->view('emails.custom-verify-email', [
+                    'url' => $url,
+                    'user' => $notifiable,
+                ]);
+
+                // $filePath = storage_path('app/public/sample.pdf');
+
+                // $mailMessage->attach($filePath, [
+                //     'as' => 'YourFile.pdf', 
+                //     'mime' => 'application/pdf',
+                // ]);
+
+            return $mailMessage;
+        });
     }
 }
